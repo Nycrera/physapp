5320a36f65fb03ee426dd7d76de1e2611e9fd714
Alperen Yurdakul <alperenyurdakul7@gmail.com>
2021-12-09 00:25:10 +0300

    fix db

--- controllers/exercises.js
+++ controllers/exercises.js
@@ -2,12 +2,13 @@ const path = require('path');
 
 module.exports = function(req, res){
     let db = require('../helpers/database')(require('../config.json'));
-    db.query('SELECT * FROM exercises WHERE disabled = FALSE', [], (err, exercises) => {
+    db.query('SELECT * FROM exercises', [], (err, exercises) => {
         db.query('SELECT * FROM videos', [], (err2, videos) => {
             if(err || err2){
                 console.log(err);
                 console.log(err2);
             }
+
             res.render(path.join(__dirname,'../pages/exercises.html'),{exercises:exercises, videos:videos});
         });
     });
