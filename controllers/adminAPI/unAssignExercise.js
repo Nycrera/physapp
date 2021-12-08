1b0b1d19575b7e482f0796957c6bc5a8e553962e
Alperen Yurdakul <alperenyurdakul7@gmail.com>
2021-12-08 21:39:37 +0300

    Create unAssignExercise.js

---
+++ controllers/adminAPI/unAssignExercise.js
@@ -0,0 +1,12 @@
+module.exports = function(req,res,next){
+    let exerciseId = req.body.exerciseId
+    let db = require('../helpers/database')(require('../config.json'));
+    db.query('UPDATE user_exercises SET disabled = TRUE WHERE id = ?',[exerciseId], (err)=>{
+        if(err){
+            console.log(err);
+            res.end(500);
+            return;
+        }
+        res.json({});
+    });
+}
