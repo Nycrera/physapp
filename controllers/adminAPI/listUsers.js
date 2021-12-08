e99d7a18aae8ac7b2fcaab84085ca4116e7b4c17
Alperen Yurdakul <alperenyurdakul7@gmail.com>
2021-12-08 22:58:02 +0300

    Update listUsers.js

--- controllers/adminAPI/listUsers.js
+++ controllers/adminAPI/listUsers.js
@@ -10,7 +10,7 @@ module.exports = function (req, res, next) {
         }
         result.forEach((user,index) => {
             var moment = require('moment');
-            result[index].register_date = moment(result[index].register_date.local().format("DD-MM-YYYY hh:mm:ss");
+            result[index].register_date = moment(result[index].register_date).local().format("DD-MM-YYYY hh:mm:ss");
             result[index].lastlogin = moment(result[index].lastlogin).local().format("DD-MM-YYYY hh:mm:ss");
             getProgress(db, user.id, (percentage) => {
                 usersProcessed++;
