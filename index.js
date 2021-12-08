e9324a88671368bb698cde8e4a7b19b7adb02c72
Alperen Yurdakul <alperenyurdakul7@gmail.com>
2021-12-08 21:45:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var multer = require('multer');
 
 app.set('trust proxy', 'loopback');
 app.set('view engine', 'ejs');
-app.use(session({ secret: 'Keep it secret', name: 'uniqueSessionID', saveUninitialized: false }));
+app.use(session({ secret: 'Keep it secret', resave: true,name: 'uniqueSessionID', saveUninitialized: false }));
 
 const admin_auth = require('./middleware/adminAuth');
 const auth = require('./middleware/auth');
@@ -33,9 +33,9 @@ app.get('/admin_dashboard', admin_auth, (req, res) => {
 });
 
 app.get('/registerUser',admin_auth,require('./controllers/register')[0]);
-app.post('/registerUser',admin_auth, express.urlencoded(), require('./controllers/register')[1]);
+app.post('/registerUser',admin_auth, express.urlencoded({ extended: true }), require('./controllers/register')[1]);
 app.get('/editUser', admin_auth, require('./controllers/editUser')[0]);
-app.post('/editUser', admin_auth, express.urlencoded(), require('./controllers/editUser')[1]);
+app.post('/editUser', admin_auth, express.urlencoded({ extended: true }), require('./controllers/editUser')[1]);
 app.get('/exercises', admin_auth, require('./controllers/exercises'));
 app.get('/viewprofile', admin_auth, require('./controllers/viewprofile'));
 
@@ -63,10 +63,10 @@ app.get('/userAPI/assignedExercises', auth, require('./controllers/userAPI/getEx
 app.post('/userAPI/updateStatus', auth, express.json(), require('./controllers/userAPI/updateExerciseStatus'));
 
 // Log in and Log out routes
-app.post('/admin_login', express.urlencoded(), require("./controllers/adminLogin")[1]);
+app.post('/admin_login', express.urlencoded({ extended: true }), require("./controllers/adminLogin")[1]);
 app.get('/admin_login', require("./controllers/adminLogin")[0]);
 
-app.post('/login', express.urlencoded(), require("./controllers/login")[1]);
+app.post('/login', express.urlencoded({ extended: true }), require("./controllers/login")[1]);
 app.get('/login', require("./controllers/login")[0]);
 app.get('/logout', require("./controllers/logout"));
 
