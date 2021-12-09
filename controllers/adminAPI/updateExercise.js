55ce8a3292396d1583d21d3049a6a05c0a59caa4
Alperen Yurdakul <alperenyurdakul7@gmail.com>
2021-12-09 03:10:15 +0300

    Update updateExercise.js

--- controllers/adminAPI/updateExercise.js
+++ controllers/adminAPI/updateExercise.js
@@ -1,7 +1,7 @@
 module.exports = function(req, res, next){
     let db = require('../../helpers/database')(require('../../config.json'));
-    db.query('UPDATE exercises SET name = ? ,video = ? WHERE id = ?', [req.body.data.name, req.body.data.video, req.body.id], (err) => {
-        db.query('SELECT * FROM exercises WHERE id = ? WHERE disabled = FALSE', [req.body.id], (err2, result2) => {
+    db.query('UPDATE exercises SET name = ? , video = ? WHERE id = ?', [req.body.data.name, req.body.data.video, req.body.id], (err) => {
+        db.query('SELECT * FROM exercises WHERE id = ?', [req.body.id], (err2, result2) => {
             if(err || err2){
                 console.log(err);
                 console.log(err2);
