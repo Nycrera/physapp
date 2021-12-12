ac339e59ff0261485ec7f545f8c71cf45012ff2d
Alperen Yurdakul <alperenyurdakul7@gmail.com>
2021-12-12 15:20:11 +0300

    Update database.js

--- helpers/database.js
+++ helpers/database.js
@@ -1,11 +1,10 @@
 module.exports = function(config){
     var mysql      = require('mysql');
-    var connection = mysql.createConnection({
+    var pool = mysql.createPool({
       host     : config.db.host,
       user     : config.db.user,
       password : config.db.password,
       database : config.db.database
     });
-    connection.connect();
-    return connection;    
+    return pool;  
 }
